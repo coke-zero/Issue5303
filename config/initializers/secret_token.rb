5d04bc6116ceecfb332a7bcefff98522e5c3c44d
Tadas Tamosauskas <tadas@pdfcv.com>
2012-05-02 18:02:53 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RelationSerialization::Application.config.secret_token = '08de4bd5b532de91461a7e15ef69dd579dc18819882e590a40382d011e73c1dc1080818089c4f1ebc281de1d5956679ea7dc6489389d90e407493cee1bb1f93d'
